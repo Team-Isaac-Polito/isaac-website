e0152011ee81b49fb25d7280c15d018ab7ec89d5
Cristina <cristinagenduso77@gmail.com>
2023-05-29 12:27:58 +0200

    [General] added fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,15 @@
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		// fontFamily: {
-		//   'raleway': ['raleway'],
+		fontFamily: {
+			raleway: ["raleway"],
+			Staatliches: ["staatliches"],
+		},
 		extend: {
 			colors: {
 				"light-blue": "#549697",
 				"dark-blue": "#1b1d44",
-				"yellow": "#ffc757",
-				
+				yellow: "#ffc757",
 			},
 		},
 	},
