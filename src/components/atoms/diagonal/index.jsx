bbdb6f99af10444f5e9890b8a5f962166295f3c3
giorgia22 <g.tallino@libero.it>
2023-06-05 01:25:33 +0200

    Added line at the end of paragraph

--- src/components/atoms/diagonal/index.jsx
+++ src/components/atoms/diagonal/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 export default function Diagonal(props) {
+    
 	return props.isTopDown ? (
         <div>
         <svg width="1920" height="153" viewBox="0 0 1920 153" className={props.secondColor}>
@@ -9,7 +10,10 @@ export default function Diagonal(props) {
         <div className="mt-[-153px] z-2">
         <svg width="1920" height="153" viewBox="0 0 1920 153" className={props.firstColor}>
         <path d="M1920 0H0L1920 153V0Z" />
-        </svg></div></div>
+        </svg></div>
+        {props.isEnd? (<div className="z-2 mt-[-120px]"><svg width="1920" height="161" viewBox="0 0 1920 161" fill="none" className={props.lineColor}>
+                        <path d="M1921.5 5L-1.5 156" stroke="#1B1D44" stroke-width="10"/></svg></div>):""}
+        </div>
     
     ) :
     (
@@ -20,7 +24,10 @@ export default function Diagonal(props) {
         <div className="mt-[-153px] z-2">
         <svg width="1920" height="153" viewBox="0 0 1920 153" className={props.firstColor}>
         <path d="M0 0H1920L0 153V0Z" />
-        </svg></div></div>
+        </svg></div>
+        {props.isEnd? (<div className="bg-white z-2 mt-[-120px]"><svg width="1920" height="161" viewBox="0 0 1920 161" fill="none" className={props.lineColor}>
+                        <path d="M1921.5 5L-1.5 156" stroke="#1B1D44" stroke-width="10"/></svg></div>):""}
+        </div>
 
     );
     }
