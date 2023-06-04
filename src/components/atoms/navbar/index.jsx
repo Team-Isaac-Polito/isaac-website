2b63e6d8bdeff64eebd3c07f60521a1efb6fe480
giorgia22 <g.tallino@libero.it>
2023-06-05 00:47:37 +0200

    Some margin changes and grouped some characteristics of the pages writings in the parent div (text size and uppercase)

--- src/components/atoms/navbar/index.jsx
+++ src/components/atoms/navbar/index.jsx
@@ -3,23 +3,23 @@ import Logo from "../../../assets/logo.svg";
 
 export default function Navbar(props) {
 	return (
-		<div className="grid grid-cols-3 font-Staatliches h-[150px] bg-dark-blue z-10 w-full">
-			<img src={Logo} alt="Logo" className="m-auto ml-28" />
-			<nav className="flex flex-row items-center m-auto w-fit">
-				<div className="text-white m-[20px] text-5xl active:text-yellow">
+		<div className="grid grid-cols-3 font-Staatliches h-[130px] bg-dark-blue z-10 w-full">
+			<img src={Logo} alt="Logo" className="m-auto ml-28 h-[80px]" />
+			<nav className="flex flex-row items-center m-auto w-fit text-5xl uppercase">
+				<div className="text-white m-[20px] active:text-yellow">
 					<a href="/">HOME</a>
 				</div>
-				<div className="text-white m-[20px] text-5xl">
+				<div className="text-white m-[20px]">
 					<a href="/about">ABOUT</a>
 				</div>
-				<div className="text-white m-[20px] text-5xl">
+				<div className="text-white m-[20px]">
 					<a href="/progetti">PROGETTI</a>
 				</div>
-				<div className="text-white uppercase m-[20px] text-5xl">
+				<div className="text-white m-[20px]">
 					<a href="/eventi">eventi</a>
 				</div>
 			</nav>
-			<div className="flex flex-row text-white w-fit text-[40px] absolute right-28 mt-11">
+			<div className="flex flex-row text-white w-fit text-[40px] absolute right-28 mt-8">
 				{" "}
 				<div className="m-20[px]">
 					<a href="">ITA</a>
