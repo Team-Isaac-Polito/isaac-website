81684eaf030c2163c8cf9647eec051b1ce038cee
Cristina <cristinagenduso77@gmail.com>
2023-06-09 22:44:44 +0200

    Fixed RotatedBorder component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,10 @@ import RotatedBorder from "../components/atoms/RotatedBorder"
 import Hero from "../components/molecules/Hero"
 import Title from "../components/atoms/Title"
 import lightbulb from "../assets/lightbulb.png"
-import Logo from "../assets/svg/logo.svg"
 
 export default function Home() {
   return (
     <div>
-      {/* <Logo /> */}
       <Hero
         title="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
         subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
@@ -27,34 +25,23 @@ export default function Home() {
           <div className="w-[130px] h-[130px] laptop:w-[180px] laptop:h-[180px] desktop:w-[262px] desktop:h-[263px] bg-gray-400 rounded-[10px] m-auto"></div>
         </div>
       </div>
-      <div className="relative py-10">
+      <div className="mt-5">
         <RotatedBorder
+          rotateBorder="-93deg"
           borderHeight="90%"
           color="red"
           width="150px"
           height="230px"
-        />
-        <div className="!overflow-visible m-auto">
-          <img src={lightbulb} className="w-[130px] m-auto" alt="lightbulb" />
-        </div>
+        >
+          <div className="!overflow-visible -ml-3">
+            <img
+              src={lightbulb}
+              className="w-[130px] relative left-[15%] top-[7px]"
+              alt="lightbulb"
+            />
+          </div>
+        </RotatedBorder>
       </div>
-      {/* <Paragraph
-        title="Progetti"
-        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget laoreet arcu, et placerat erat. Proin semper tristique elit a dictum. Donec id erat sapien. Morbi non placerat lorem. Aenean feugiat est id nisi suscipit, sed iaculis dolor fermentum. Donec mauris elit, aliquam ac lacinia et, mollis in ante. Maecenas enim nibh, vehicula id volutpat eu."
-      />
-      <div className="mt-40">
-        <Paragraph
-          isTextLeft
-          title="Progetti"
-          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget laoreet arcu, et placerat erat. Proin semper tristique elit a dictum. Donec id erat sapien. Morbi non placerat lorem. Aenean feugiat est id nisi suscipit, sed iaculis dolor fermentum. Donec mauris elit, aliquam ac lacinia et, mollis in ante. Maecenas enim nibh, vehicula id volutpat eu."
-        />
-      </div> */}
-      {/* <Diagonal
-				isEnd
-				firstColor="fill-dark-blue-isaac"
-				secondColor="fill-white"
-				lineColor="stroke-dark-blue-isaac"
-			/> */}
     </div>
   )
 }
