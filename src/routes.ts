93835108594df3e8c011af61b3612de801e50f73
Cristina <cristinagenduso77@gmail.com>
2023-06-09 19:29:31 +0200

    Updated path to be absolute

--- src/routes.ts
+++ src/routes.ts
@@ -2,25 +2,25 @@ export const navRoutes = [
   {
     id: 0,
     title: "Home",
-    path: "/",
+    path: "./",
     en: true,
   },
   {
     id: 1,
     title: "Chi siamo",
-    path: "/about",
+    path: "about",
     en: true,
   },
   {
     id: 2,
     title: "Progetti",
-    path: "/projects",
+    path: "projects",
     en: true,
   },
   {
     id: 3,
     title: "Eventi",
-    path: "/events",
+    path: "events",
     en: true,
   },
 ]
