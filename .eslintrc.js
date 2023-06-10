86d6b6fcadd3e78dbc4663912b147c54053382a4
Cristina <cristinagenduso77@gmail.com>
2023-06-10 18:51:52 +0200

    Solved prettier conflict with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,14 @@
 module.exports = {
-    root: true,
-    extends: [
-        'react-app',
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended'
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: { 
-         project: ['./tsconfig.json'],
-         warnOnUnsupportedTypeScriptVersion: false
-    }
+  root: true,
+  extends: [
+    "react-app",
+    "prettier",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["./tsconfig.json"],
+    warnOnUnsupportedTypeScriptVersion: false,
+  },
 }
