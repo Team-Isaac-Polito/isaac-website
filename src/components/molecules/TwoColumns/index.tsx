cda06e9f2c674d6f1ee64cbd91836491b9c314fa
giorgia22 <g.tallino@libero.it>
2023-06-11 21:08:16 +0200

    Fixed button (border, text, palette)

--- src/components/molecules/TwoColumns/index.tsx
+++ src/components/molecules/TwoColumns/index.tsx
@@ -8,11 +8,11 @@ const TwoColumns: FC<TwoColumnsProps> = (props) => {
   return props.isTextLeft ? (
     <div className={props.heigth}>
       <div className="grid grid-cols-2 gap-20 w-full h-full">
-        <div className=" w-fit m-auto">
+        <div className=" w-fit m-auto mx-10">
           <Title className={props.palette}>{props.title}</Title>
-          <div className="my-10 w-[700px]">{props.text}</div>
+          <div className="my-10 ">{props.text}</div>
           {props.buttonText == undefined || props.buttonText == "" ? null : (
-            <Button text={props.buttonText} className={props.palette} />
+            <Button palette={props.palette}>{props.buttonText}</Button>
           )}
         </div>
         <RotatedBorderNew palette={props.palette} borderSize={props.borderSize}>
@@ -26,13 +26,13 @@ const TwoColumns: FC<TwoColumnsProps> = (props) => {
         <RotatedBorderNew palette={props.palette} borderSize={props.borderSize}>
           {props.children}
         </RotatedBorderNew>
-        <div className=" w-fit text-right m-auto">
+        <div className=" w-fit text-right m-auto mx-10">
           <Title className={props.palette}>{props.title}</Title>
-          <div className="my-10 w-[700px]">{props.text}</div>
+          <div className="my-10">{props.text}</div>
 
           <div className="w-full pr-0">
             {props.buttonText == undefined || props.buttonText == "" ? null : (
-              <Button text={props.buttonText} className={props.palette} />
+              <Button palette={props.palette}>{props.buttonText}</Button>
             )}
           </div>
         </div>
