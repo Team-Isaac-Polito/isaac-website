51d9a00eb7e664f9cfc03bf254dd85eb90d318d4
Cristina <cristinagenduso77@gmail.com>
2023-06-12 11:39:59 +0200

    Projects page fix import and components props

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,5 +1,4 @@
 import React from "react"
-import Button from "../components/atoms/Button"
 import Title from "../components/atoms/Title"
 import Paragraph from "../components/molecules/Paragraph"
 import reseq from "../assets/prova.mp4"
@@ -9,43 +8,45 @@ export default function Projects(): JSX.Element {
   return (
     <div>
       <div>
-        <Title className="py-7 text-center bg-white text-dark-blue">
+        <Title className="text-center bg-white py-7 text-dark-blue">
           Rese.Q Mk1
         </Title>
 
-        <video width="1920" height="600" autoPlay={true} muted loop>
+        <video width="1920" height="600" autoPlay muted loop>
           <source src={reseq} type="video/mp4" />
         </video>
 
-        <Paragraph palette="bluePalette">
+        <Paragraph className="bluePalette">
           <TwoColumns
             isTextLeft
-            palette=" bluePalette "
+            classNameTitle="bluePalette"
+            classNameButton="bluePalette"
             title="chi è rese.q mk1"
             text="È il primo prototipo del team per raggiungere l’obiettivo di creare un robot innovativo per le operazioni SAR.
             La nostra idea è quella di avere un robot modulare cingolato e con due possibili configurazioni: braccio robotico e robot strisciante. In modo che riesca a destreggiarsi facilmente in situazioni pericolose, per perlustrare l’ambiente senza rischiare le vite dei soccorritori."
             buttonText="Learn more"
-            borderSize=" tablet:w-[400px] tablet:h-[500px] desktop:w-[619px] desktop:h-[729px] "
-            heigth=" desktop:h-[1100px] tablet:h-[650px] "
+            classNameBorder="tablet:w-[400px] tablet:h-[500px] desktop:w-[619px] desktop:h-[729px]"
+            className="desktop:h-[1100px] tablet:h-[650px]"
           >
             <div className="rounded-md bg-gray-500 desktop:w-[547px] desktop:h-[666px] tablet:w-[340px] tablet:h-[460px]" />
           </TwoColumns>
         </Paragraph>
 
         <TwoColumns
-          heigth="h-[800px]"
-          palette="whitePalette"
-          borderSize="h-0 w-0"
+          className="h-[800px]"
+          classNameButton="whitePalette"
+          classNameTitle="whitePalette"
+          classNameBorder="h-0 w-0"
           title="caratteristiche"
           text="Struttura modulare:
-Permette di adattare il robot a diverse scenari, con moduli specializzati
-Configurazione snake-like: Permette maggiore agilità nel movimento
-Configurazione braccio robotico: Per un sollevamento in verticale di parte del robot
-Controllo remoto:
-Tramite un telecomando e una telecamera sul robot, con possibilità future di guida autonoma e semi-autonoma"
+          Permette di adattare il robot a diverse scenari, con moduli specializzati
+          Configurazione snake-like: Permette maggiore agilità nel movimento
+          Configurazione braccio robotico: Per un sollevamento in verticale di parte del robot
+          Controllo remoto:
+          Tramite un telecomando e una telecamera sul robot, con possibilità future di guida autonoma e semi-autonoma"
         >
           <div className="w-fit">
-            <div className="flex flex-row rounded-2xl  bg-light-blue-isaac h-fit mb-6 py-3 w-full m-auto">
+            <div className="flex flex-row w-full py-3 m-auto mb-6 rounded-2xl bg-light-blue-isaac h-fit">
               <h2 className="ml-4 font-extrabold text-white uppercase align-left">
                 Scheda tecnica
               </h2>
@@ -126,8 +127,8 @@ Tramite un telecomando e una telecamera sul robot, con possibilità future di gu
 
         <Paragraph
           invertSlope
-          palette="lightBluePalette"
-          className="py-[100px] mb-[50px]"
+          className="lightBluePalette"
+          classNameDiv="py-[100px] mb-[50px]"
         >
           <div className="m-auto mx-[200px] text-center">
             <Title className="lightBluePalette mb-[20px]">obiettivi</Title>
