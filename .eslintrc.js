1d22758a3fce574c2772e82a6e5085f959bfb85b
Cristina <cristinagenduso77@gmail.com>
2023-06-12 11:37:13 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,12 @@ module.exports = {
   root: true,
   extends: [
     "react-app",
-    "prettier",
     "eslint:recommended",
+    "prettier",
     "plugin:@typescript-eslint/recommended",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: ["./tsconfig.json"],
     warnOnUnsupportedTypeScriptVersion: false,
   },
 }
