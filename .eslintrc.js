ea7d6a18556b4944b7a24a11fc4af22eb85e7628
Marco Fincato <marcofincato777@gmail.com>
2023-06-13 23:02:31 +0200

    New stricter ESlint, non dependent on CRA

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,13 @@
 module.exports = {
   root: true,
   extends: [
-    "react-app",
     "eslint:recommended",
-    "prettier",
+    "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
   ],
   parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
   parserOptions: {
     warnOnUnsupportedTypeScriptVersion: false,
   },
