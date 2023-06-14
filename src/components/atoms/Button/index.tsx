e67811af60c19be5da0d3cdbe88bf8fd294d7d89
Marco Fincato <marcofincato777@gmail.com>
2023-06-14 02:15:12 +0200

    Replaced forwardRef with FC

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,32 +1,36 @@
-import React, { forwardRef } from "react"
+import React, { FC } from "react"
 import ButtonProps from "./index.types"
 import classNames from "classnames"
 
-const Button = forwardRef<HTMLAnchorElement, ButtonProps>(
-  ({ isPrimary, children, className, href, onClick }, ref): JSX.Element => {
-    return isPrimary ? (
+const Button: FC<ButtonProps> = ({
+  isPrimary,
+  children,
+  className,
+  href,
+  onClick,
+}): JSX.Element => {
+  return isPrimary ? (
+    <a
+      href={href}
+      onClick={onClick}
+      className="bg-light-blue-isaac cursor-pointer px-5 py-3 text-white text-base tablet:text-xl laptop:text-2xl notebook:text-3xl flex flex-col items-center justify-center font-extrabold w-[220px] h-[45px] tablet:w-[170px] tablet:h-[35px] laptop:w-[220px] laptop:h-[55px] notebook:w-[270px] notebook:h-[55px] desktop:w-[320px] desktop:h-[72px] rounded-3xl tablet:rounded-md laptop:rounded-xl transition duration-500 ease-in-out uppercase m-auto hover:scale-105 hover:-translate-y-1"
+    >
+      <div>{children}</div>
+    </a>
+  ) : (
+    <div>
       <a
         href={href}
         onClick={onClick}
-        className="bg-light-blue-isaac cursor-pointer px-5 py-3 text-white text-base tablet:text-xl laptop:text-2xl notebook:text-3xl flex flex-col items-center justify-center font-extrabold w-[220px] h-[45px] tablet:w-[170px] tablet:h-[35px] laptop:w-[220px] laptop:h-[55px] notebook:w-[270px] notebook:h-[55px] desktop:w-[320px] desktop:h-[72px] rounded-3xl tablet:rounded-md laptop:rounded-xl transition duration-500 ease-in-out uppercase m-auto hover:scale-105 hover:-translate-y-1"
+        className={classNames(
+          "px-5 py-3 cursor-pointer flex flex-col items-center justify-center text-base tablet:text-xl laptop:text-2xl notebook:text-3xl font-extrabold w-[220px] h-[45px] tablet:w-[170px] tablet:h-[35px] laptop:w-[220px] laptop:h-[55px] notebook:w-[270px] notebook:h-[55px] desktop:w-[320px] desktop:h-[72px] rounded-3xl tablet:rounded-md laptop:rounded-xl transition duration-500 ease-in-out uppercase m-auto hover:scale-105 hover:-translate-y-1 border-solid border-2",
+          className
+        )}
       >
-        <div>{children}</div>
+        {children}
       </a>
-    ) : (
-      <div>
-        <a
-          href={href}
-          onClick={onClick}
-          className={classNames(
-            "px-5 py-3 cursor-pointer flex flex-col items-center justify-center text-base tablet:text-xl laptop:text-2xl notebook:text-3xl font-extrabold w-[220px] h-[45px] tablet:w-[170px] tablet:h-[35px] laptop:w-[220px] laptop:h-[55px] notebook:w-[270px] notebook:h-[55px] desktop:w-[320px] desktop:h-[72px] rounded-3xl tablet:rounded-md laptop:rounded-xl transition duration-500 ease-in-out uppercase m-auto hover:scale-105 hover:-translate-y-1 border-solid border-2",
-            className
-          )}
-        >
-          {children}
-        </a>
-      </div>
-    )
-  }
-)
+    </div>
+  )
+}
 
 export default Button
--- src/components/atoms/Title/index.tsx
+++ src/components/atoms/Title/index.tsx
@@ -1,22 +1,20 @@
-import React, { forwardRef } from "react"
+import React, { FC } from "react"
 import TitleProps from "./index.types"
 import classNames from "classnames"
 
-const Title = forwardRef<HTMLAnchorElement, TitleProps>(
-  ({ children, className }, ref): JSX.Element => {
-    return (
-      <div className="text-[30px] laptop:text-4xl desktop:text-[60px] uppercase font-extrabold">
-        <h1
-          className={classNames(
-            "text-base tablet:text-xl laptop:text-2xl notebook:text-3xl font-extrabold",
-            className
-          )}
-        >
-          {children}
-        </h1>
-      </div>
-    )
-  }
-)
+const Title: FC<TitleProps> = ({ children, className }): JSX.Element => {
+  return (
+    <div className="text-[30px] laptop:text-4xl desktop:text-[60px] uppercase font-extrabold">
+      <h1
+        className={classNames(
+          "text-base tablet:text-xl laptop:text-2xl notebook:text-3xl font-extrabold",
+          className
+        )}
+      >
+        {children}
+      </h1>
+    </div>
+  )
+}
 
 export default Title
