f859f7d5cdce24fd1c2900995b0b67d5e3ad9cc3
Cristina <cristinagenduso77@gmail.com>
2023-06-18 16:47:50 +0200

    Index update to typescript and added i18next

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { I18nextProvider } from "react-i18next"
+import { BrowserRouter } from "react-router-dom"
+import i18n from "../i18n"
+import "../i18n.ts"
+import App from "./App"
+import "./styles/global.css"
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <I18nextProvider i18n={i18n}>
+        <App />
+      </I18nextProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+)
