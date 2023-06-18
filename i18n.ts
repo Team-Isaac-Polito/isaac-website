b0be9a1fb7d7b11cfe032bcf14f9dc8ccfde27c3
Cristina <cristinagenduso77@gmail.com>
2023-06-18 16:46:51 +0200

    Create i18n config file

---
+++ i18n.ts
@@ -0,0 +1,25 @@
+import i18n from "i18next"
+import Backend from "i18next-http-backend"
+import { initReactI18next } from "react-i18next"
+i18n
+  .use(Backend)
+  .use(initReactI18next)
+  .init({
+    debug: true,
+    backend: {
+      loadPath: "/locales/{{lng}}/{{ns}}.json",
+    },
+    supportedLngs: ["it", "en"],
+    interpolation: {
+      escapeValue: false,
+      formatSeparator: ".",
+    },
+    lng: "it",
+    fallbackLng: "en",
+    detection: {
+      order: ["path"],
+      lookupFromPathIndex: 0,
+    },
+  })
+
+export default i18n
