9d596f43ca04bddbcdb8b61135858b299d59d6f7
giorgia22 <g.tallino@libero.it>
2023-07-01 01:14:16 +0200

    Deleted m-auto on button

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -23,7 +23,7 @@ const Button: FC<ButtonProps> = ({
         href={href}
         onClick={onClick}
         className={classNames(
-          "px-5 py-3 cursor-pointer flex flex-col items-center justify-center text-base tablet:text-xl laptop:text-2xl notebook:text-3xl font-extrabold w-[220px] h-[45px] tablet:w-[170px] tablet:h-[35px] laptop:w-[220px] laptop:h-[55px] notebook:w-[270px] notebook:h-[55px] desktop:w-[320px] desktop:h-[72px] rounded-3xl tablet:rounded-md laptop:rounded-xl transition duration-500 ease-in-out uppercase m-auto hover:scale-105 hover:-translate-y-1 border-solid border-2",
+          "px-5 py-3 cursor-pointer flex flex-col items-center justify-center text-base tablet:text-xl laptop:text-2xl notebook:text-3xl font-extrabold w-[220px] h-[45px] tablet:w-[170px] tablet:h-[35px] laptop:w-[220px] laptop:h-[55px] notebook:w-[270px] notebook:h-[55px] desktop:w-[320px] desktop:h-[72px] rounded-3xl tablet:rounded-md laptop:rounded-xl transition duration-500 ease-in-out uppercase hover:scale-105 hover:-translate-y-1 border-solid border-2",
           className
         )}
       >
