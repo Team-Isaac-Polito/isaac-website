7e2b7ed84f95a43f3582dd8b41d3c802435fb95a
Cristina <cristinagenduso77@gmail.com>
2023-07-18 18:24:18 +0200

    Hero height fix

--- src/components/molecules/Hero/index.tsx
+++ src/components/molecules/Hero/index.tsx
@@ -4,9 +4,9 @@ import HeroProps from "./index.types"
 
 const Hero: FC<HeroProps> = (props) => {
   return (
-    <div className="bg-black h-[1095px] pt-52">
-      <div className="m-auto text-center text-white w-fit ">
-        <h3 className="w-[370px] m-auto">{props.subtitle}</h3>
+    <div className="bg-black h-fit py-52">
+      <div className="m-auto text-center text-white w-fit">
+        <h3 className="w-[370px] m-auto text-3xl">{props.subtitle}</h3>
         <h1 className="text-[70px] leading-[82px] font-semibold w-[590px] pt-10">
           {props.title}
         </h1>
