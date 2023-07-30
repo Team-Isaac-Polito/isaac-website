8c042ddbbb831155cd45c3e589f3dd152719e507
Cristina <cristinagenduso77@gmail.com>
2023-07-30 11:00:45 +0200

    Button minor fix

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -13,7 +13,7 @@ const Button: FC<ButtonProps> = ({
     <a
       href={href}
       onClick={onClick}
-      className="bg-light-blue-isaac cursor-pointer px-5 py-3 text-white text-base tablet:text-xl laptop:text-2xl notebook:text-3xl desktop:text-4xl flex flex-col items-center justify-center font-extrabold w-[220px] h-[45px] tablet:w-[170px] tablet:h-[35px] laptop:w-[220px] laptop:h-[55px] notebook:w-[270px] notebook:h-[55px] desktop:w-[320px] desktop:h-[72px] rounded-3xl tablet:rounded-md laptop:rounded-xl transition duration-500 ease-in-out uppercase m-auto hover:scale-105 hover:-translate-y-1"
+      className="bg-light-blue-isaac cursor-pointer px-5 py-3 text-white text-base tablet:text-xl laptop:text-2xl notebook:text-3xl desktop:text-4xl flex flex-col items-center justify-center font-extrabold w-[220px] h-[45px] tablet:w-[170px] tablet:h-[35px] laptop:w-[220px] laptop:h-[55px] notebook:w-[270px] notebook:h-[55px] desktop:w-[320px] desktop:h-[72px] rounded-3xl tablet:rounded-md laptop:rounded-xl desktop:rounded-lg transition duration-500 ease-in-out uppercase m-auto hover:scale-105 hover:-translate-y-1"
     >
       <div>{children}</div>
     </a>
@@ -23,7 +23,7 @@ const Button: FC<ButtonProps> = ({
         href={href}
         onClick={onClick}
         className={classNames(
-          "cursor-pointer flex flex-col items-center justify-center text-base tablet:text-xl laptop:text-2xl notebook:text-3xl desktop:text-4xl font-semibold w-fit h-fit py-4 px-16 rounded-3xl tablet:rounded-md laptop:rounded-xl desktop:rounded-[20px] transition duration-500 ease-in-out uppercase hover:scale-105 hover:-translate-y-1 border-solid border-[5px]",
+          "cursor-pointer flex flex-col items-center justify-center text-base tablet:text-xl laptop:text-2xl notebook:text-3xl desktop:text-4xl font-semibold w-fit h-fit py-4 px-16 rounded-3xl tablet:rounded-md laptop:rounded-xl desktop:rounded-2xl transition duration-500 ease-in-out uppercase hover:scale-105 hover:-translate-y-1 border-solid border-[5px]",
           className
         )}
       >
