5836d1cdb9f5d59b37872bc1fc34c6a38b565458
giorgia22 <g.tallino@libero.it>
2023-08-15 16:43:56 +0200

    Shrinked the code of the Paragraph

--- src/components/molecules/Paragraph/index.tsx
+++ src/components/molecules/Paragraph/index.tsx
@@ -9,43 +9,33 @@ const Paragraph: FC<ParagraphProps> = ({
   className,
   palette,
 }) => {
-  return invertSlope ? (
-    isLineTop ? (
-      <div className={classNames("skew-y-3", className)}>
-        <div className={classNames("h-3 mb-7", palette)} />
-        <div className={palette}>
-          <div className="py-32 mx-48 text-4xl text-center -skew-y-3 desktop:py-40 h-fit">
-            {children}
-          </div>
-        </div>
-      </div>
-    ) : (
-      <div className={classNames("skew-y-3", className)}>
-        <div className={palette}>
-          <div className="py-32 mx-48 text-4xl text-center -skew-y-3 desktop:py-40 h-fit">
-            {children}
-          </div>
-        </div>
-        <div className={classNames("h-3 mt-7", palette)} />
-      </div>
-    )
-  ) : isLineTop ? (
-    <div className={classNames("-skew-y-3", className)}>
-      <div className={classNames("h-3 mb-7", palette)} />
-      <div className={palette}>
-        <div className="py-32 mx-48 text-4xl text-center skew-y-3 desktop:py-40 h-fit">
-          {children}
-        </div>
+  const slopeParagraph = invertSlope ? "skew-y-3" : "-skew-y-3"
+  const slopeContent = invertSlope ? "-skew-y-3" : "skew-y-3"
+  const lineMargin = isLineTop ? " mb-7 " : " mt-7 "
+
+  const slope = (
+    <div className={palette}>
+      <div
+        className={
+          "py-32 mx-48 text-4xl text-center desktop:py-40 h-fit " + slopeContent
+        }
+      >
+        {children}
       </div>
     </div>
+  )
+
+  const line = <div className={"h-3 " + palette + lineMargin} />
+
+  return isLineTop ? (
+    <div className={classNames(slopeParagraph, className)}>
+      {line}
+      {slope}
+    </div>
   ) : (
-    <div className={classNames("-skew-y-3", className)}>
-      <div className={palette}>
-        <div className="py-32 mx-48 text-4xl text-center skew-y-3 desktop:py-40 h-fit">
-          {children}
-        </div>
-      </div>
-      <div className={classNames("h-3 mt-7", palette)} />
+    <div className={classNames(slopeParagraph, className)}>
+      {slope}
+      {line}
     </div>
   )
 }
