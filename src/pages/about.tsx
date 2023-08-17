1b891ec0e069ec1f34dcb1bb3ed9810d7d3c4b2b
Cristina <cristinagenduso77@gmail.com>
2023-08-17 19:29:11 +0200

    Added areas section to about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,27 @@
 import Hero from "@molecules/Hero"
+import Paragraph from "@molecules/Paragraph"
+import TwoColumns from "@molecules/TwoColumns"
+import Areas from "@organisms/Areas"
 import React from "react"
 
 export default function About(): JSX.Element {
   return (
     <>
       <Hero title={"About"} />
+      <Paragraph className="laptop:my-32" palette="whitePalette">
+        <TwoColumns
+          isTextLeft
+          palette="whitePalette"
+          title="su di noi"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget laoreet arcu, et placerat erat. Proin semper tristique elit a dictum. Donec id erat sapien. Morbi non placerat lorem. Aenean feugiat est id nisi suscipit, sed iaculis dolor fermentum. Donec mauris elit, aliquam ac lacinia et, mollis in ante. Maecenas enim nibh, vehicula id volutpat eu."
+          classNameBorder="tablet:w-[400px] tablet:h-[500px] desktop:w-[619px] desktop:h-[729px] border-light-blue-isaac"
+        >
+          <div className="rounded-md bg-gray-300 desktop:w-[547px] desktop:h-[666px] tablet:w-[240px] tablet:h-[360px]" />
+        </TwoColumns>
+      </Paragraph>
+      <Paragraph palette={"lightBluePalette"} invertSlope>
+        <Areas />
+      </Paragraph>
     </>
   )
 }
