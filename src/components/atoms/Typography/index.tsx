ababf8f1ad99fc8f2a8cf00eac11874727a1b988
giorgia22 <g.tallino@libero.it>
2023-08-20 16:27:15 +0200

    Shrinked Typography code

--- src/components/atoms/Typography/index.tsx
+++ src/components/atoms/Typography/index.tsx
@@ -1,76 +1,46 @@
-import classNames from "classnames"
 import React, { FC } from "react"
 import TypographyProps from "./index.types"
 
 const Typography: FC<TypographyProps> = ({ variant, className, children }) => {
+  let textCharacteristics = ""
+
   switch (variant) {
     case "hero":
-      return (
-        <div
-          className={classNames(
-            "antialiased text-center font-extrabold text-4xl tablet:text-5xl laptop:text-6xl notebook:text-7xl desktop:text-8xl",
-            className ?? ""
-          )}
-        >
-          {children}
-        </div>
-      )
+      textCharacteristics =
+        "antialiased text-center font-extrabold text-4xl tablet:text-5xl laptop:text-6xl notebook:text-7xl desktop:text-8xl"
+      break
+
     case "h1":
-      return (
-        <div
-          className={classNames(
-            "uppercase antialiased font-extrabold text-3xl tablet:text-2xl laptop:text-4-5xl notebook:text-5-5xl desktop:text-7xl",
-            className ?? ""
-          )}
-        >
-          {children}
-        </div>
-      )
+      textCharacteristics =
+        "uppercase antialiased font-extrabold text-3xl tablet:text-2xl laptop:text-4-5xl notebook:text-5-5xl desktop:text-7xl"
+      break
+
     case "h2":
-      return (
-        <div
-          className={classNames(
-            "font-bold text-2xl laptop:text-3xl notebook:text-4xl desktop:text-5xl",
-            className ?? ""
-          )}
-        >
-          {children}
-        </div>
-      )
+      textCharacteristics =
+        "font-bold text-2xl laptop:text-3xl notebook:text-4xl desktop:text-5xl"
+      break
+
     case "p":
-      return (
-        <div
-          className={classNames(
-            "antialiased text-xl tablet:text-base laptop:text-2xl notebook:text-3xl desktop:text-4-5xl desktop:leading-tight font-normal",
-            className ?? ""
-          )}
-        >
-          {children}
-        </div>
-      )
+      textCharacteristics =
+        "antialiased text-xl tablet:text-base laptop:text-2xl notebook:text-3xl desktop:text-4-5xl desktop:leading-tight font-normal"
+      break
+
     case "label":
-      return (
-        <div
-          className={classNames(
-            "antialiased font-normal text-xs tablet:text-lg notebook:text-2xl desktop:text-4xl",
-            className ?? ""
-          )}
-        >
-          {children}
-        </div>
-      )
+      textCharacteristics =
+        "antialiased font-normal text-xs tablet:text-lg notebook:text-2xl desktop:text-4xl"
+      break
+
     case "footer":
-      return (
-        <div
-          className={classNames(
-            "font-normal text-[9px] tablet:text-[8px] laptop:text-[10px] notebook:text-sm desktop:text-xl",
-            className ?? ""
-          )}
-        >
-          {children}
-        </div>
-      )
+      textCharacteristics =
+        "font-normal text-[9px] tablet:text-[8px] laptop:text-[10px] notebook:text-sm desktop:text-xl"
+      break
   }
+
+  return (
+    <div className={textCharacteristics + " " + className ?? ""}>
+      {children}
+    </div>
+  )
 }
 
 export default Typography
