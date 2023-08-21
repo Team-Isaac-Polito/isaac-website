47d76262e45c8af988ff666537b5f13da7c5fcdd
Cristina <cristinagenduso77@gmail.com>
2023-08-21 20:28:17 +0200

    scroll to top on route change

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
 import Layout from "@organisms/Layout"
-import React, { Suspense } from "react"
-import { Route, Routes } from "react-router-dom"
+import React, { Suspense, useLayoutEffect } from "react"
+import { Route, Routes, useLocation } from "react-router-dom"
 import Home from "./pages"
 import NotFound from "./pages/NotFound"
 import About from "./pages/about"
 import Events from "./pages/events"
 import Projects from "./pages/projects"
 
+const Wrapper = ({ children }) => {
+  const location = useLocation()
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo(0, 0)
+  }, [location.pathname])
+  return children
+}
+
 const App = () => {
   return (
     <Suspense fallback="loading">
-      <Routes>
-        <Route path={import.meta.env.BASE_URL} element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="projects" element={<Projects />} />
-          <Route path="events" element={<Events />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
+      <Wrapper>
+        <Routes>
+          <Route path={import.meta.env.BASE_URL} element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="projects" element={<Projects />} />
+            <Route path="events" element={<Events />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </Wrapper>
     </Suspense>
   )
 }
