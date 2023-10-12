6e6c27e10f481d71ff076086f86ba1635d316872
Cristina <cristinagenduso77@gmail.com>
2023-10-12 09:35:50 +0200

    Added about description

--- src/components/atoms/Isaac/index.tsx
+++ src/components/atoms/Isaac/index.tsx
@@ -11,11 +11,11 @@ function Isaac() {
         variant="p"
         className="mx-10 tablet:mx-[75px] laptop:mx-[100px] desktop:mx-[200px] w-fit tablet:text-center"
       >
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget
-        laoreet arcu, et placerat erat. Proin semper tristique elit a dictum.
-        Donec id erat sapien. Morbi non placerat lorem. Aenean feugiat est id
-        nisi suscipit, sed iaculis dolor fermentum. Donec mauris elit, aliquam
-        ac lacinia et, mollis in ante.{" "}
+        ISAAC è un team di studenti del Politecnico di Torino che opera nel
+        campo della robotica. Inizialmente è iniziato con l&apos;obiettivo di
+        sviluppare un robot umanoide. Il team ha subito un cambio di rotta nel
+        2017 quando il Prof. Quaglia è diventato il nuovo tutor accademico. Ora
+        le attività del team si concentrano sui robot di servizio mobile.
       </Typography>
       <div className="grid grid-rows-3 gap-6 tablet:gap-0 tablet:grid-rows-1 tablet:grid-cols-3 py-20 mx-20 laptop:mx-40 desktop:mx-[500px] desktop:gap-20">
         <div className="w-[130px] h-[130px] laptop:w-[180px] laptop:h-[180px] notebook:w-[230px] notebook:h-[230px] desktop:w-[280px] desktop:h-[280px] bg-gray-400 rounded-[10px] m-auto"></div>
