0c8e2da3687a521c67f4d4881adc5052cd59627f
Luca De Paola <60526200+luca-byte@users.noreply.github.com>
2024-03-19 22:48:04 +0100

    feat(i18n): Add generic translations of titles in homepage

--- src/components/atoms/Form/index.tsx
+++ src/components/atoms/Form/index.tsx
@@ -1,11 +1,14 @@
 import Typography from "@atoms/Typography"
+import { useTranslation } from "react-i18next"
 import React from "react"
 
 const Form = () => {
+  const { t } = useTranslation()
+
   return (
     <div className="mt-40">
       <Typography variant={"h1"} className="ml-96">
-        Scrivici
+        {t("contact-us")}
       </Typography>
       <iframe
         src="https://tally.so/embed/m6jLY5?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
--- src/components/molecules/Hero/index.tsx
+++ src/components/molecules/Hero/index.tsx
@@ -1,9 +1,12 @@
 import Button from "@atoms/Button"
 import Typography from "@atoms/Typography"
 import React, { FC } from "react"
+import { useTranslation } from "react-i18next"
 import HeroProps from "./index.types"
 
 const Hero: FC<HeroProps> = (props) => {
+  const { t } = useTranslation()
+
   return (
     <div className="relative py-24 bg-center bg-no-repeat bg-cover bg-hero laptop:py-32 laptop:h-[500px] notebook:h-[550px] desktop:py-52">
       <div className="bg-[#1e1e1e] w-full laptop:h-[500px] notebook:h-[550px] absolute top-0 opacity-30" />
@@ -18,7 +21,7 @@ const Hero: FC<HeroProps> = (props) => {
           {props.title}
         </Typography>
         <div className="m-auto mt-16 w-fit">
-          <Button isPrimary>Contattaci</Button>
+          <Button isPrimary>{t("contact-us")}</Button>
         </div>
       </div>
     </div>
