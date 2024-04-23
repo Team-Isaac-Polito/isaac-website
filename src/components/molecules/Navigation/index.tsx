cc344a9ede161765297afdc181b334837938abe3
cristinagenduso <83962372+cristinagenduso@users.noreply.github.com>
2024-04-23 11:19:40 +0200

    Added translation button to Navigation on mobile

--- src/components/molecules/Navigation/index.tsx
+++ src/components/molecules/Navigation/index.tsx
@@ -1,6 +1,8 @@
 import { SocialNavigation } from "@atoms/SocialNavigation"
+import Typography from "@atoms/Typography"
 import { AnimatePresence, LazyMotion, m } from "framer-motion"
-import React, { FC } from "react"
+import React, { FC, useState } from "react"
+import { useTranslation } from "react-i18next"
 import { NavLink } from "react-router-dom"
 import { ReactComponent as Logo } from "../../../assets/svg/logo.svg"
 import { navRoutes } from "../../../routes"
@@ -9,6 +11,22 @@ import NavigationProps from "./index.types"
 const features = () => import("../../../features").then((res) => res.default)
 
 const Navigation: FC<NavigationProps> = ({ toggleMenu, setToggleMenu }) => {
+  const { i18n } = useTranslation()
+  const onToggleLanguageClick = (lng: string) => {
+    i18n.changeLanguage(lng)
+    handleButtonClick()
+    setToggleMenu(!toggleMenu)
+  }
+  const changeTo = i18n.language === "it" ? "en" : "it"
+
+  const [isVisible, setIsVisible] = useState(false)
+  const [buttonStyle, setButtonStyle] = useState("")
+
+  const handleButtonClick = () => {
+    setIsVisible(!isVisible)
+    setButtonStyle(!isVisible ? "rounded-r-none" : "")
+  }
+
   return (
     <>
       <AnimatePresence>
@@ -21,51 +39,91 @@ const Navigation: FC<NavigationProps> = ({ toggleMenu, setToggleMenu }) => {
               transition={{ duration: 0.8, ease: [0.6, 0.05, -0.01, 0.9] }}
               className="fixed top-0 w-full h-full text-white z-[100]"
             >
-              <div className="flex flex-col h-full bg-black">
-                <div className="flex flex-row justify-between mt-3">
-                  <div className="mt-4 ml-10">
+              <div className="grid h-full px-6 bg-[#0f0f0f] grid-rows-auto">
+                <div className="grid grid-cols-2 mt-3">
+                  <div className="">
                     <Logo className="h-8 w-fit" />
                   </div>
-                  <div className="" onClick={() => setToggleMenu(!toggleMenu)}>
+                  <div
+                    className="justify-self-end"
+                    onClick={() => setToggleMenu(!toggleMenu)}
+                  >
                     <button className="border-none outline-none cursor-pointer bg-none">
-                      <span className="text-white text-2xl font-extrabold hover:text-yellow-isaac transition duration-[250ms] mr-3">
+                      <span className="text-white text-2xl font-extrabold hover:text-yellow-isaac transition duration-[250ms]">
                         &#x2715;
                       </span>
                     </button>
                   </div>
                 </div>
-                <div className="flex h-[200px] mt-[100px] flex-col tablet:flex-row">
-                  <nav className="flex items-center h-full pl-10">
+                <div className="flex h-[200px] mt-28 flex-col tablet:flex-row">
+                  <nav className="flex items-center h-full">
                     <ul>
                       {navRoutes.map((route) => {
                         return (
                           <m.li key={route.id} className="list-none">
-                            <NavLink to={route.path}>
-                              <button
-                                onClick={() => setToggleMenu(!toggleMenu)}
+                            <button onClick={() => setToggleMenu(!toggleMenu)}>
+                              <m.div
+                                className="antialiased flex items-center text-[30px] tracking-normal text-white hover:text-yellow-isaac transition duration-[250ms] w-full last:mb-[2vh]"
+                                whileHover={{
+                                  x: 40,
+                                  transition: {
+                                    duration: 0.25,
+                                    ease: [0.6, 0.05, -0.01, 0.9],
+                                  },
+                                }}
                               >
-                                <m.div
-                                  className="antialiased flex items-center text-[30px] tracking-normal text-white hover:text-yellow-isaac transition duration-[250ms] w-full last:mb-[2vh]"
-                                  whileHover={{
-                                    x: 40,
-                                    transition: {
-                                      duration: 0.25,
-                                      ease: [0.6, 0.05, -0.01, 0.9],
-                                    },
-                                  }}
+                                <NavLink
+                                  to={route.path}
+                                  className={({ isActive }) =>
+                                    isActive ? "underline" : ""
+                                  }
                                 >
                                   {route.title}
-                                </m.div>
-                              </button>
-                            </NavLink>
+                                </NavLink>
+                              </m.div>
+                            </button>
                           </m.li>
                         )
                       })}
                     </ul>
                   </nav>
                 </div>
-                <div className="flex pb-3 pl-10 mt-20 tablet:hidden">
+                <div className="relative grid w-16 grid-cols-2 mt-8 tablet:hidden">
+                  <button
+                    onClick={handleButtonClick}
+                    className={`w-8 h-8 m-auto font-bold text-center text-black uppercase bg-white rounded-full ${buttonStyle}`}
+                  >
+                    {i18n.language}
+                  </button>
+                  {isVisible && (
+                    <ul className="flex my-auto text-black">
+                      <li>
+                        <button
+                          className="block w-8 h-8 uppercase bg-gray-100 rounded-r-full hover:bg-gray-300 hover:font-bold"
+                          onClick={() => onToggleLanguageClick(changeTo)}
+                        >
+                          {changeTo}
+                        </button>
+                      </li>
+                    </ul>
+                  )}
+                </div>
+                <div className="grid grid-cols-2 mt-16">
                   <SocialNavigation />
+                  <div className="text-right">
+                    <Typography variant="h2" className="font-semibold">
+                      Privacy
+                    </Typography>
+                    <NavLink to="/documents/privacy-policy.pdf">
+                      <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-sm underline"
+                      >
+                        Privacy
+                      </a>
+                    </NavLink>
+                  </div>
                 </div>
               </div>
             </m.div>
