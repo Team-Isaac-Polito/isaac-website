e30b032b48f41cf4c14c87981c8d0ff9d034e85e
cristinagenduso <83962372+cristinagenduso@users.noreply.github.com>
2024-04-23 18:08:38 +0200

    Removed RotatedBorderProps component

--- src/components/organisms/GalleryEvent/index.tsx
+++ src/components/organisms/GalleryEvent/index.tsx
@@ -1,4 +1,3 @@
-import RotatedBorder from "@atoms/RotatedBorder"
 import Typography from "@atoms/Typography"
 import Gallery from "@molecules/Gallery"
 import Paragraph from "@molecules/Paragraph"
@@ -18,43 +17,35 @@ const GalleryEvent: FC<GalleryEventProps> = ({ title, desc, palette }) => {
           </Typography>
           <Typography variant="p">{desc}</Typography>
           <div className="my-14 laptop:mt-32 laptop:mb-5 desktop:my-40">
-            <RotatedBorder
-              width="90%"
-              height="90%"
-              left="5%"
-              top="5%"
-              className="border-yellow-isaac rotate-[-3deg]"
-            >
-              <Gallery
-                images={[
-                  {
-                    children: (
-                      <div className="desktop:w-[370px] desktop:h-[370px] notebook:w-[300px] notebook:h-[300px] laptop:w-[220px] laptop:h-[220px] w-[170px] h-[170px] bg-gray-300 m-auto rounded-xl" />
-                    ),
-                  },
-                  {
-                    children: (
-                      <div className="desktop:w-[370px] desktop:h-[370px] notebook:w-[300px] notebook:h-[300px] laptop:w-[220px] laptop:h-[220px] w-[170px] h-[170px] bg-gray-300 m-auto rounded-xl" />
-                    ),
-                  },
-                  {
-                    children: (
-                      <div className="desktop:w-[370px] desktop:h-[370px] notebook:w-[300px] notebook:h-[300px] laptop:w-[220px] laptop:h-[220px] w-[170px] h-[170px] bg-gray-300 m-auto rounded-xl" />
-                    ),
-                  },
-                  {
-                    children: (
-                      <div className="desktop:w-[370px] desktop:h-[370px] notebook:w-[300px] notebook:h-[300px] laptop:w-[220px] laptop:h-[220px] w-[170px] h-[170px] bg-gray-300 m-auto rounded-xl" />
-                    ),
-                  },
-                  {
-                    children: (
-                      <div className="desktop:w-[370px] desktop:h-[370px] notebook:w-[300px] notebook:h-[300px] laptop:w-[220px] laptop:h-[220px] w-[170px] h-[170px] bg-gray-300 m-auto rounded-xl" />
-                    ),
-                  },
-                ]}
-              />
-            </RotatedBorder>
+            <Gallery
+              images={[
+                {
+                  children: (
+                    <div className="desktop:w-[370px] desktop:h-[370px] notebook:w-[300px] notebook:h-[300px] laptop:w-[220px] laptop:h-[220px] w-[170px] h-[170px] bg-gray-300 m-auto rounded-xl" />
+                  ),
+                },
+                {
+                  children: (
+                    <div className="desktop:w-[370px] desktop:h-[370px] notebook:w-[300px] notebook:h-[300px] laptop:w-[220px] laptop:h-[220px] w-[170px] h-[170px] bg-gray-300 m-auto rounded-xl" />
+                  ),
+                },
+                {
+                  children: (
+                    <div className="desktop:w-[370px] desktop:h-[370px] notebook:w-[300px] notebook:h-[300px] laptop:w-[220px] laptop:h-[220px] w-[170px] h-[170px] bg-gray-300 m-auto rounded-xl" />
+                  ),
+                },
+                {
+                  children: (
+                    <div className="desktop:w-[370px] desktop:h-[370px] notebook:w-[300px] notebook:h-[300px] laptop:w-[220px] laptop:h-[220px] w-[170px] h-[170px] bg-gray-300 m-auto rounded-xl" />
+                  ),
+                },
+                {
+                  children: (
+                    <div className="desktop:w-[370px] desktop:h-[370px] notebook:w-[300px] notebook:h-[300px] laptop:w-[220px] laptop:h-[220px] w-[170px] h-[170px] bg-gray-300 m-auto rounded-xl" />
+                  ),
+                },
+              ]}
+            />
           </div>
         </>
       </Paragraph>
