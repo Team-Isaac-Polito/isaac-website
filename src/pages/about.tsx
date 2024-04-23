4e67874078f9debb29b18d25393885a62df85678
cristinagenduso <83962372+cristinagenduso@users.noreply.github.com>
2024-04-23 14:16:24 +0200

    About minor fixes

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -6,6 +6,7 @@ import Areas from "@organisms/Areas"
 import History from "@organisms/History"
 import React from "react"
 import { useTranslation } from "react-i18next"
+import img from "../assets/lightbulb.png"
 
 export default function About(): JSX.Element {
   const { t } = useTranslation("about")
@@ -19,22 +20,18 @@ export default function About(): JSX.Element {
           palette="whitePalette"
           title="su di noi"
           text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget laoreet arcu, et placerat erat. Proin semper tristique elit a dictum. Donec id erat sapien. Morbi non placerat lorem. Aenean feugiat est id nisi suscipit, sed iaculis dolor fermentum. Donec mauris elit, aliquam ac lacinia et, mollis in ante. Maecenas enim nibh, vehicula id volutpat eu."
-          src={""}
-          alt={""}
+          src={img}
+          alt={"immagine di esempio"}
         />
       </Paragraph>
       <Paragraph className="laptop:my-32" palette="bluePalette">
-        <>
-          <History />
-        </>
+        <History />
       </Paragraph>
       <Paragraph className="notebook:my-32" palette="whitePalette">
-        <>
-          <Typography variant="h1" className="mb-20 text-light-blue-isaac">
-            {t("areas.title")}
-          </Typography>
-          <Typography variant="p">{t("areas.description")}</Typography>
-        </>
+        <Typography variant="h1" className="mb-20 text-light-blue-isaac">
+          {t("areas.title")}
+        </Typography>
+        <Typography variant="p">{t("areas.description")}</Typography>
       </Paragraph>
       <Paragraph palette={"lightBluePalette"} invertSlope>
         <Areas />
