1c92ac12b388ccb16aeea5a5f5917805ee769f68
cristinagenduso <83962372+cristinagenduso@users.noreply.github.com>
2024-06-05 12:07:13 +0200

    mk22 & mk23 added all images in events page

--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -3,6 +3,15 @@ import GalleryEvent from "@organisms/GalleryEvent"
 import StaticEvent from "@organisms/StaticEvent"
 import React from "react"
 import { useTranslation } from "react-i18next"
+import mk22_1 from "../assets/events/mk22/mf22_1.jpg"
+import mk22_2 from "../assets/events/mk22/mk22_2.jpg"
+import mk22_3 from "../assets/events/mk22/mk22_3.jpg"
+import mk22_4 from "../assets/events/mk22/mk22_4.jpg"
+import mk22_5 from "../assets/events/mk22/mk22_5.jpg"
+import mk23_1 from "../assets/events/mk23/mk23_1.jpg"
+import mk23_3 from "../assets/events/mk23/mk23_3.jpg"
+import mk23_4 from "../assets/events/mk23/mk23_4.jpg"
+import mk23_5 from "../assets/events/mk23/mk23_5.jpg"
 
 interface Event {
   type: string
@@ -25,6 +34,32 @@ export default function Events(): JSX.Element {
               title={event.title}
               desc={event.desc}
               palette={event.palette}
+              images={[
+                {
+                  src: "",
+                  alt: "",
+                },
+                {
+                  src: mk23_1,
+                  alt: "",
+                },
+                {
+                  src: mk23_3,
+                  alt: "",
+                },
+                {
+                  src: mk23_4,
+                  alt: "",
+                },
+                {
+                  src: "",
+                  alt: "",
+                },
+                {
+                  src: mk23_5,
+                  alt: "",
+                },
+              ]}
             />
           )
         } else if (event.type === "gallery") {
@@ -34,6 +69,28 @@ export default function Events(): JSX.Element {
               title={event.title}
               desc={event.desc}
               palette={event.palette}
+              images={[
+                {
+                  src: mk22_1,
+                  alt: "",
+                },
+                {
+                  src: mk22_3,
+                  alt: "",
+                },
+                {
+                  src: mk22_4,
+                  alt: "",
+                },
+                {
+                  src: mk22_2,
+                  alt: "",
+                },
+                {
+                  src: mk22_5,
+                  alt: "",
+                },
+              ]}
             />
           )
         }
