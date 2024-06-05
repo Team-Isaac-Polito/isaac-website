18ede908d4dc6f4157c0376212e6410a9a106e31
cristinagenduso <83962372+cristinagenduso@users.noreply.github.com>
2024-06-05 13:16:05 +0200

    Form removed deprecated prop

--- src/components/atoms/Form/index.tsx
+++ src/components/atoms/Form/index.tsx
@@ -7,7 +7,6 @@ const Form = () => {
         src="https://tally.so/embed/m6jLY5?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
         width="60%"
         height="350"
-        frameBorder={0}
         className="m-auto"
       />
     </div>
