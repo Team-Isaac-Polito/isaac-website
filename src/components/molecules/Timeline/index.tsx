8c050a68906232710607886ef567ecb985023345
cristinagenduso <83962372+cristinagenduso@users.noreply.github.com>
2024-06-05 12:33:40 +0200

    Timeline is now centered on 2017

--- src/components/molecules/Timeline/index.tsx
+++ src/components/molecules/Timeline/index.tsx
@@ -5,7 +5,7 @@ import SegmentedControlProps from "./index.types"
 const SegmentedControl: FC<SegmentedControlProps> = ({
   segments,
   callback,
-  defaultIndex = 0,
+  defaultIndex,
   controlRef,
 }) => {
   const [activeIndex, setActiveIndex] = useState(defaultIndex)
@@ -17,8 +17,8 @@ const SegmentedControl: FC<SegmentedControlProps> = ({
 
   return (
     <div ref={controlRef}>
-      <div className="relative z-0 grid grid-cols-2 grid-rows-2 gap-8 m-auto bg-transparent tablet:gap-4 notebook:gap-8 tablet:grid-cols-5 tablet:grid-rows-1 desktop:p-3">
-        {segments?.map((item, i) => (
+      <div className="relative z-0 grid grid-cols-2 grid-rows-2 gap-8 m-auto bg-transparent tablet:gap-4 notebook:gap-8 tablet:grid-cols-7 tablet:grid-rows-1 desktop:p-3">
+        {segments!.map((item, i) => (
           <div
             key={item.value}
             ref={item.ref}
