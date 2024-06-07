a63d32f1f2243d7e27149ad90efb2ffcb0857f68
cristinagenduso <83962372+cristinagenduso@users.noreply.github.com>
2024-06-07 13:24:26 +0200

    New tally form design

--- src/components/atoms/Form/index.tsx
+++ src/components/atoms/Form/index.tsx
@@ -4,9 +4,9 @@ const Form = () => {
   return (
     <div className="mt-14 laptop:mt-20 desktop:mt-32">
       <iframe
-        src="https://tally.so/embed/m6jLY5?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
+        src="https://tally.so/embed/3xQyky?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
         width="60%"
-        height="350"
+        height="550"
         className="m-auto"
       />
     </div>
