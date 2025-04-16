f3a265183a561c4ec5af1b2335e1bad1b55b11b9
matinz03 <74014122+matinz03@users.noreply.github.com>
2025-04-17 00:37:59 +0200

    fix: Fix sidebar and language reload issues (#326)
    * Fix language reset on page reload
    * Fix sidebar crash while visible on page reload

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,6 +1,7 @@
 import classNames from "classnames"
 import React, { FC } from "react"
 import ButtonProps from "./index.types"
+import { Link } from "react-router-dom"
 
 const Button: FC<ButtonProps> = ({
   isPrimary,
@@ -9,22 +10,22 @@ const Button: FC<ButtonProps> = ({
   className,
 }): JSX.Element => {
   return isPrimary ? (
-    <a
-      href={href}
+    <Link
+      to={href}
       className="flex items-center py-2 text-base font-semibold uppercase transition duration-500 ease-in-out cursor-pointer w-fit h-fit notebook:py-3 px-7 laptop:px-8 notebook:px-10 desktop:px-12 rounded-3xl tablet:rounded-xl desktop:rounded-2xl hover:scale-105 hover:-translate-y-1 bg-light-blue-isaac tablet:text-base laptop:text-xl notebook:text-2xl desktop:text-4xl"
     >
       {children}
-    </a>
+    </Link>
   ) : (
-    <a
-      href={href}
+    <Link
+      to={href}
       className={classNames(
         "w-fit h-fit py-2 text-base font-semibold uppercase cursor-pointer notebook:py-3 px-7 laptop:px-8 notebook:px-10 desktop:px-12 tablet:text-base laptop:text-xl notebook:text-2xl desktop:text-4xl border-solid border-4 notebook:border-[5px] rounded-3xl tablet:rounded-xl desktop:rounded-2xl transition duration-500 ease-in-out",
         className
       )}
     >
       {children}
-    </a>
+    </Link>
   )
 }
 
--- src/components/atoms/NavBar/index.tsx
+++ src/components/atoms/NavBar/index.tsx
@@ -6,7 +6,7 @@ import Logo from "../../../assets/svg/logo.svg?react"
 import { navRoutes } from "../../../routes"
 import NavBarProps from "./index.types"
 
-const NavBar: FC<NavBarProps> = ({ setToggleMenu, toggleMenu }) => {
+const NavBar: FC<NavBarProps> = ({ setToggleMenu }) => {
   const { t, i18n } = useTranslation()
 
   const changeLanguage = (lng: string) => {
@@ -21,9 +21,9 @@ const NavBar: FC<NavBarProps> = ({ setToggleMenu, toggleMenu }) => {
 
   return (
     <header className="sticky top-0 z-20 grid w-full grid-cols-5 text-white h-14 tablet:h-16 laptop:h-20 font-Staatliches notebook:h-24 desktop:h-28 bg-dark-blue-isaac tablet:backdrop-blur-md">
-      <a href="/" className="m-auto">
+      <NavLink to="/" className="m-auto">
         <Logo className="ml-4 h-10 w-28 laptop:w-fit laptop:ml-14 notebook:ml-20 desktop:ml-28 laptop:h-12 notebook:h-16 desktop:h-20 flex" />
-      </a>
+      </NavLink>
       <nav className="items-center hidden w-full col-span-3 m-auto text-5xl uppercase tablet:block">
         <ul className="grid w-full grid-cols-5">
           {navRoutes.map((route) => {
@@ -65,7 +65,7 @@ const NavBar: FC<NavBarProps> = ({ setToggleMenu, toggleMenu }) => {
       </div>
       <div className="h-6 col-start-5 mt-4 mr-4 w-fit justify-self-end tablet:hidden">
         <button
-          onClick={() => setToggleMenu(!toggleMenu)}
+          onClick={() => setToggleMenu((toggleMenu) => !toggleMenu)}
           className="block w-6 h-6 cursor-pointer bg-none"
         >
           <img alt="Menu" src={MenuIcon} />
