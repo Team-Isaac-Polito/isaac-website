e7135d8dfb232a805057e10471b1353f5b06005c
Matinz03 <matinpsn1@gmail.com>
2025-05-21 18:40:25 +0200

    fix: removed events.json

--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -9,22 +9,21 @@ import { GalleryImage, EventType } from "@organisms/GalleryEvent/events.types"
 const palette = ["bluePalette", "lightBluePalette"]
 
 export default function Events(): JSX.Element {
-  const { t, i18n } = useTranslation("events")
+  const { i18n } = useTranslation("events")
   const { events, isLoading } = useEvents()
+  function isEn(): boolean {
+    return i18n.language === "en"
+  }
   if (isLoading) return <FaSpinner />
 
   return (
     <>
-      <Hero title={t("title")} />
+      <Hero title={isEn() ? "Events" : "Eventi"} />
       {events.map((event: EventType, index: number) => (
         <GalleryEvent
           key={event.id}
           title={event.title}
-          desc={
-            i18n.language === "en"
-              ? event.description_eng
-              : event.description_ita
-          }
+          desc={isEn() ? event.description_eng : event.description_ita}
           palette={index % 2 ? palette[0] : palette[1]}
           images={event.gallery.map((image: GalleryImage) => ({
             src: `https://cms.teamisaac.it/assets/${image.directus_files_id}?width=1900&height=2800&quality=80`,
