dd47e8025739f3d97003fccd641290fea82fb138
Salvatore Calo <dafder45678@gmail.com>
2026-02-20 16:46:06 +0100

    fix: changed from static sponsor to dynamic

--- src/components/molecules/Sponsors/index.tsx
+++ src/components/molecules/Sponsors/index.tsx
@@ -1,33 +1,27 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { useTranslation } from "react-i18next"
 import Typography from "@atoms/Typography"
-import polito from "@assets/sponsors/PoliTo.png"
-import preRi from "@assets/sponsors/preziosiRicami.png"
-import stm from "@assets/sponsors/STM.png"
-import npcb from "@assets/sponsors/NextPCB.png"
 
-const sponsors = [
-  { name: "Politecnico di Torino", logo: polito, url: "https://www.polito.it" },
-  {
-    name: "Preziosi Ricami",
-    logo: preRi,
-    url: "http://www.preziosiricami.it/",
-  },
-  {
-    name: "STMicroelectronics",
-    logo: stm,
-    url: "https://www.st.com",
-  },
-  {
-    name: "NextPCB",
-    logo: npcb,
-    url: "https://www.nextpcb.com/",
-  },
-]
+interface SponsorInterface {
+  id: number
+  name: string
+  url: string
+  logo: string
+}
 
 const Sponsors = () => {
   const { t } = useTranslation("homepage")
+  const [sponsors, setSponsors] = React.useState<SponsorInterface[]>([])
 
+  useEffect(() => {
+    async function getSponsors() {
+      const data = await fetch("https://cms.teamisaac.it/items/sponsor")
+      const sponsorsData = await data.json()
+      setSponsors(sponsorsData.data)
+    }
+    getSponsors()
+  }, [sponsors])
+  console.log(sponsors)
   return (
     <div className="mx-6 mt-20 bg-white notebook:mt-32 laptop:mt-24 text-dark-blue-isaac h-fit tablet:mx-20 laptop:mx-28 desktop:mx-52">
       <Typography className="m-auto mb-10 text-center" variant="h1">
@@ -44,7 +38,7 @@ const Sponsors = () => {
           >
             <div className="flex items-center justify-center w-full h-full max-w-xs max-h-64 tablet:max-w-[300px] tablet:max-h-[200px]">
               <img
-                src={sponsor.logo}
+                src={"https://cms.teamisaac.it/assets/" + sponsor.logo}
                 alt={sponsor.name}
                 className="max-w-full max-h-full justify-center"
               />
